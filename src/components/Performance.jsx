61316f3101fb7987ce7646648a509bdb29e5a43b
MartinHanacik <hanacik.m@gmail.com>
2020-11-01 20:17:49 +0100

    Add package-lock.json to gitignore Add module textfit

--- src/components/Performance.jsx
+++ src/components/Performance.jsx
@@ -4,6 +4,10 @@ import Box from "@material-ui/core/Box";
 import { Textfit } from 'react-textfit';
 
 function Performance({performance, setSelectedPerformance}) {
+    const maxFontSizeName = 50;
+    const maxFontSizeTitle = 40;
+    const maxFontSizeInfo = 20;
+
     return (
         <Box className='performance-item-wrapper bold t-white overflow pos-rel fullHeight hand'>
             <Box className='performance-item' onClick={() => setSelectedPerformance(performance)}>
@@ -12,7 +16,7 @@ function Performance({performance, setSelectedPerformance}) {
                 }
                 <Box className='overlay performance-overlay pos-abs t-center'>
                     <Box className="performance-name fullHeight padd text-fit">
-                        <Textfit max={50}>
+                        <Textfit max={maxFontSizeName}>
                             {performance.name}
                         </Textfit>
                     </Box>
@@ -20,14 +24,14 @@ function Performance({performance, setSelectedPerformance}) {
                         <Grid container direction='column' justify="center" alignItems="center">
                             <Grid item xs={7} className="fullWidth">
                                 <Box className="fullHeight text-fit performance-overlay-title t-red">
-                                    <Textfit max={40}>
+                                    <Textfit max={maxFontSizeTitle}>
                                         {performance.additionalData.title}
                                     </Textfit>
                                 </Box>
                             </Grid>
                             <Grid item xs={5} className="fullWidth">
                                 <Box className="fullHeight text-fit performance-overlay-info">
-                                    <Textfit max={20}>
+                                    <Textfit max={maxFontSizeInfo}>
                                         {performance.additionalData.info}<br></br>
                                         {performance.additionalData.note}
                                     </Textfit>
